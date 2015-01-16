f67cc657c853edb3e505e03c0f95e852211971b4
Aaron Dandy <aaron.dandy@live.com>
2015-01-15 23:56:12 -0700

    added stylecop

---
+++ src/Wharrgarbl/IO/FilePath.cs
@@ -0,0 +1,12 @@
+﻿namespace Wharrgarbl.IO
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class FilePath : FileSystemPath
+    {
+    }
+}
---
+++ src/Wharrgarbl/IO/FileSystemPath.cs
@@ -0,0 +1,13 @@
+﻿namespace Wharrgarbl.IO
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public abstract class FileSystemPath
+    {
+    }
+}
