c14a02bf38fb64a62ecfd3ca86cdbc1750efc3b6
Aaron Dandy <aaron.dandy@live.com>
2015-01-18 11:24:17 -0700

    adding some IO extensions for FileSystemInfo #8

---
+++ src/Wharrgarbl/CoreExtensions/IOEx.cs
@@ -0,0 +1,60 @@
+﻿namespace Wharrgarbl.CoreExtensions
+{
+    using System;
+    using System.IO;
+
+    public static class IOEx
+    {
+        public static DirectoryInfo ToDirectoryInfo(this string path)
+        {
+            return new DirectoryInfo(path);
+        }
+
+        public static FileInfo ToFileInfo(this string path)
+        {
+            return new FileInfo(path);
+        }
+
+        public static DirectoryInfo Subdirectory(this DirectoryInfo directory, string relativePath)
+        {
+            var combined = Combine(directory, relativePath);
+            return new DirectoryInfo(combined);
+        }
+
+        public static DirectoryInfo Subdirectory(this DirectoryInfo directory, params string[] relativePaths)
+        {
+            var combined = Combine(directory, relativePaths);
+            return new DirectoryInfo(combined);
+        }
+
+        public static FileInfo File(this DirectoryInfo directory, string relativePath)
+        {
+            var combined = Combine(directory, relativePath);
+            return new FileInfo(combined);
+        }
+
+        public static FileInfo File(this DirectoryInfo directory, params string[] relativePaths)
+        {
+            var combined = Combine(directory, relativePaths);
+            return new FileInfo(combined);
+        }
+
+        private static string Combine(this DirectoryInfo directory, string relativePath)
+        {
+            return Path.Combine(directory.FullName, relativePath);
+        }
+
+        private static string Combine(this DirectoryInfo directory, params string[] relativePaths)
+        {
+            return Path.Combine(BuildPathParts(directory, relativePaths));
+        }
+
+        private static string[] BuildPathParts(DirectoryInfo directory, params string[] relativePaths)
+        {
+            var pathPaths = new string[relativePaths.Length + 1];
+            pathPaths[0] = directory.FullName;
+            Array.Copy(relativePaths, 0, pathPaths, 1, relativePaths.Length);
+            return pathPaths;
+        }
+    }
+}
---
+++ tests/Wharrgarbl.Tests/CoreExtensions/IOExtensionsFacts.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wharrgarbl.Tests.CoreExtensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using FluentAssertions;
+    using Wharrgarbl.CoreExtensions;
+    using Xunit;
+    using System.IO;
+
+    public static class IOExtensionsFacts
+    {
+        [Fact]
+        public static void can_construct_directory_info()
+        {
+            var expected = new DirectoryInfo("./");
+
+            var actual = "./".ToDirectoryInfo();
+
+            actual.FullName.Should().Be(expected.FullName);
+        }
+
+        [Fact]
+        public static void can_construct_file_info()
+        {
+            var expected = new FileInfo("./README.md");
+
+            var actual = "./README.md".ToFileInfo();
+
+            actual.FullName.Should().Be(expected.FullName);
+        }
+
+        [Fact]
+        public static void can_ref_sub_dir()
+        {
+            var expected = new DirectoryInfo("subDir");
+            var di = new DirectoryInfo("./");
+
+            var actual = di.Subdirectory("subDir");
+
+            actual.FullName.Should().Be(expected.FullName);
+        }
+
+        [Fact]
+        public static void can_ref_file_in_dir()
+        {
+            var expected = new FileInfo("README.md");
+            var di = new DirectoryInfo("./");
+
+            var actual = di.File("README.md");
+
+            actual.FullName.Should().Be(expected.FullName);
+        }
+
+        [Fact]
+        public static void can_ref_multi_part_sub_dir()
+        {
+            var expected = new DirectoryInfo("subDir1/subDir2");
+            var di = new DirectoryInfo("./");
+
+            var actual = di.Subdirectory("subDir1", "subDir2");
+
+            actual.FullName.Should().Be(expected.FullName);
+        }
+
+        [Fact]
+        public static void can_ref_multi_part_file_in_dir()
+        {
+            var expected = new FileInfo("./subDir/README.md");
+            var di = new DirectoryInfo("./");
+
+            var actual = di.File("subDir", "README.md");
+
+            actual.FullName.Should().Be(expected.FullName);
+        }
+    }
+}
