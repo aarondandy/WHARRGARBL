796a78583c2625849a9798c97fef83c31e20f64f
Aaron Dandy <aaron.dandy@live.com>
2015-01-22 00:30:59 -0700

    inject usings into scripts

---
+++ src/Wharrgarbl/WharrgarblScriptPack.cs
@@ -0,0 +1,39 @@
+﻿using ScriptCs.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wharrgarbl
+{
+    public class WharrgarblScriptPack : IScriptPack
+    {
+        public IScriptPackContext GetContext()
+        {
+            return new WharrgarblScriptPackContext();
+        }
+
+        public void Initialize(IScriptPackSession session)
+        {
+            var namespaceUsings = new[] {
+                "Wharrgarbl.CoreExtensions",
+                "Wharrgarbl.Lifetimes"
+            };
+            var functionUsings = new[] {
+                "Wharrgarbl.Functions.EnumFn",
+                "Wharrgarbl.Functions.EnvFn",
+                "Wharrgarbl.Functions.Fn"
+            };
+            foreach(var @namespace in namespaceUsings.Concat(functionUsings))
+            {
+                session.ImportNamespace(@namespace);
+            }
+        }
+
+        public void Terminate()
+        {
+            ;
+        }
+    }
+}
